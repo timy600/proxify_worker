7831de8a885c8f469b818e4fbe88e1e097176e4e
Thibaut Chevée <thibaut.chevee@wpo.eu>
2020-11-26 10:51:45 +0100

    insert url into db model and script

--- model/insert_url.js
+++ model/insert_url.js
@@ -1,14 +1,12 @@
 const sqlite3 = require('sqlite3').verbose();
 
 // open database in memory
-let db = new sqlite3.Database('./proxify_urls.db');
+let db = new sqlite3.Database('./../proxify_urls.db');
 
-
-// INSERT ROWS ==============================================
-function insert_query(sql_query) {
+// INSERT ROWS
+function run_query(sql_query) {
     db.run(sql_query);
 }
-
 
 const url_list = ['https://youtube.com', 'https://en.wikipedia.org', 'https://twitter.com', 'https://facebook.com',
 'https://amazon.com', 'https://yelp.com', 'https://imdb.com', 'https://fandom.com',
@@ -20,23 +18,11 @@ const url_list = ['https://youtube.com', 'https://en.wikipedia.org', 'https://tw
 'https://wikihow.com', 'https://paris.com', 'https://madrid.com', 'https://quefaire.com'
 ]
 
-let sql = 'INSERT INTO urlStatus VALUES ( NULL, "https://reddit.com", "NEW", Null)'
-
-url_list.forEach(url => {sql = `INSERT INTO urlStatus VALUES ( NULL, "${url}", "NEW", Null)`; insert_query(sql); console.log(sql)});
-
-// SELECT ALL ROWS ==========================================
-
-sql = 'SELECT * FROM urlStatus';
-
-db.all(sql,(err, rows ) => {
-    if (err) {
-        throw err;
-    }
-    rows.forEach( row => console.log(row.url)
-    );
+url_list.forEach(url => {
+    var sql = `INSERT INTO urlStatus VALUES ( NULL, "${url}", "NEW", NULL)`;
+    run_query(sql);
+    console.log(sql)
 });
-
-
 
 // Close the database connection ==============================
 db.close((err) => {
